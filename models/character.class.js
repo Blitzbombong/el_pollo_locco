115209f2c062c2c21294f45f02769faa3320bf75
Waldemar Giesbrecht <giesbrecht-83@web.de>
2024-05-14 19:24:41 +0200

    added character walking sound

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,7 @@ class Cahracter extends MovableObject  {
     'img/2_character_pepe/2_walk/W-25.png',
     'img/2_character_pepe/2_walk/W-26.png'
    ];
+   walking_sound = new Audio('audio/running1.mp3');
 
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -23,14 +24,17 @@ class Cahracter extends MovableObject  {
     animate() {
 
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
 
             if (this.world.keyboard.LEFT && this.x > -600 ) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             this.world.camera_x = - this.x + 100;
         }, 1000 / 60);
