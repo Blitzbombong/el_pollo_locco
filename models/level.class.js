4f9d761c9950a056d2bee273ac3b99058daa5bd0
Waldemar Giesbrecht <giesbrecht-83@web.de>
2024-05-14 17:37:23 +0200

    added level and level1

---
+++ models/level.class.js
@@ -0,0 +1,16 @@
+class Level {
+    enemies;
+    clouds;
+    backgroundObject;
+    coins;
+    salsaBottle;
+    
+
+    constructor (enemies, clouds, coins, salsaBottle, backgroundObject ) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundObject = backgroundObject;
+        this.coins = coins;
+        this.salsaBottle = salsaBottle;
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,74 +1,10 @@
 class World {
     character = new Cahracter();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-
-    clouds = [
-        new Clouds('img/5_background/layers/4_clouds/1.png'),
-        new Clouds('img/5_background/layers/4_clouds/2.png'),
-        new Clouds('img/5_background/layers/4_clouds/1.png'),
-        new Clouds('img/5_background/layers/4_clouds/2.png'),
-        new Clouds('img/5_background/layers/4_clouds/1.png'),
-        new Clouds('img/5_background/layers/4_clouds/2.png')
-    ];
-
-    coins = [
-        new Coins(),
-        new Coins(),
-        new Coins(),
-        new Coins(),
-        new Coins(),
-        new Coins()
-    ];
-
-    salsaBottle = [
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle(),
-        new SalsaBottle()
-
-    ];
-
-    backgroundObject = [
-        new BackgroundObject('img/5_background/layers/air.png', -719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
-
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-
-        new BackgroundObject('img/5_background/layers/air.png', 719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
-
-        new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
-
-        new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3)
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    coins = level1.coins;
+    salsaBottle = level1.salsaBottle;
+    backgroundObject = level1.backgroundObject;
 
 
 
