e88b4f71431b9d75433f61605f590e9e9228ea73
Waldemar Giesbrecht <giesbrecht-83@web.de>
2024-07-14 15:17:20 +0200

    level update

--- models/level.class.js
+++ models/level.class.js
@@ -1,19 +1,14 @@
 class Level {
-    enemies;
-    clouds;
-    backgroundObject;
-    coins;
-    salsaBottle;
-    endboss;
-    level_end_x = 2250;
-    
+  enemies;
+  clouds;
+  backgroundObject;
+  level_end_x = 2250;
 
-    constructor (enemies, clouds, coins, salsaBottle, endboss, backgroundObject ) {
-        this.enemies = enemies;
-        this.clouds = clouds;
-        this.backgroundObject = backgroundObject;
-        this.coins = coins;
-        this.salsaBottle = salsaBottle;
-        this.endboss = endboss;
-    }
+  constructor(enemies, clouds, coins, salsaBottle, backgroundObject) {
+    this.enemies = enemies;
+    this.clouds = clouds;
+    this.backgroundObject = backgroundObject;
+    this.coins = coins;
+    this.salsaBottle = salsaBottle;
+  }
 }
