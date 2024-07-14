82fe36a4c166ee3952f0e65cfb745005109f8b6c
Waldemar Giesbrecht <giesbrecht-83@web.de>
2024-07-14 11:04:11 +0200

    add update chicken

--- levels/level1.js
+++ levels/level1.js
@@ -1,74 +1,53 @@
-const level1 = new Level(
-    [/*  
-        new Endboss(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),*/
-    ],
-    [
-        new Clouds('img/5_background/layers/4_clouds/1.png', 150),
-        new Clouds('img/5_background/layers/4_clouds/2.png', 300),
-        new Clouds('img/5_background/layers/4_clouds/1.png', 700),
-        new Clouds('img/5_background/layers/4_clouds/2.png', 1400),
-        new Clouds('img/5_background/layers/4_clouds/1.png', 2200),
-        new Clouds('img/5_background/layers/4_clouds/2.png', 2800)
-    ],
-    [
-        new Coins(400, 300, 50, 50),
-            new Coins(500, 300, 50, 50),
-            new Coins(600, 300, 50, 50),
-            new Coins(950, 200, 50, 50),
-            new Coins(1020, 150, 50, 50),
-            new Coins(1090, 200, 50, 50),
-            new Coins(1600, 160, 50, 50),
-            new Coins(1700, 220, 50, 50),
-            new Coins(1800, 280, 50, 50),
-            new Coins(2900, 180, 200, 200)
-    ],
-    [
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png'),
-        new SalsaBottle(250, 350, 80, 80, 'img/6_salsa_bottle/1_salsa_bottle_on_ground.png')
-    ],
-    [
-        new Endboss()
-    ],
-    [
-        new BackgroundObject('img/5_background/layers/air.png', -719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+let level1;
 
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
 
-        new BackgroundObject('img/5_background/layers/air.png', 719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+function initLevel()  { 
+    level1 = new Level([ 
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+        ],
+        [
+            new Clouds('img/5_background/layers/4_clouds/1.png', 150),
+            new Clouds('img/5_background/layers/4_clouds/2.png', 300),
+            new Clouds('img/5_background/layers/4_clouds/1.png', 700),
+            new Clouds('img/5_background/layers/4_clouds/2.png', 1400),
+            new Clouds('img/5_background/layers/4_clouds/1.png', 2200),
+            new Clouds('img/5_background/layers/4_clouds/2.png', 2800)
+        ],
+        [
+            new Endboss()
+        ],
+        [
+            new BackgroundObject('img/5_background/layers/air.png', -719),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
 
-        new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
+            new BackgroundObject('img/5_background/layers/air.png', 0),
+            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
 
-        new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3)
-    ]
-);
+            new BackgroundObject('img/5_background/layers/air.png', 719),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+
+            new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
+            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
+            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
+            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
+
+            new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3)
+        ]
+    );
+}
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,48 +1,54 @@
 class Chicken extends MovableObject {
-    width = 60;
-    height = 80;
-    y = 345;
-    speed = 10;
-    IMAGES_WALKING = [
-        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
-        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
-        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
-    ];
+  width = 60;
+  height = 80;
+  y = 345;
+  speed = 10;
+  chicken_sound = new Audio("audio/chiken.mp3");
 
-    offset = {
-        top: 30,
-        left: 30,
-        right: 30,
-        bottom: 30
-   }
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+  ];
 
-    
+  IMAGES_DEAD = ["img/3_enemies_chicken/chicken_normal/2_dead/dead.png"];
 
+  offset = {
+    top: 30,
+    left: 30,
+    right: 30,
+    bottom: 30,
+  };
 
+  currentImage = 0;
 
-    constructor(){
-        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
-        this.loadImages(this.IMAGES_WALKING);
+  constructor() {
+    super().loadImage("img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImage(this.IMAGES_DEAD);
+    this.x = 200 + Math.random() * 2500; // Plaziert die Chiken auf zufahlige Psition was das Math.random ausrechnet.
+    this.speed = 0.15 + Math.random() * 0.25;
 
-        this.x = 200 + Math.random() * 2500; // Plaziert die Chiken auf zufahlige Psition was das Math.random ausrechnet.
-        this.speed = 0.15 + Math.random() * 0.25;
+    this.animate();
+    this.imgAnimate();
+  }
 
-        this.animate();
-    }
+  animate() {
+    // Walking Left
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
+  }
 
-
-    animate() {
-        // Walking Left
-        setInterval(() => {   
-            this.moveLeft();
-        }, 1000 / 60);
-
-        setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6; => 1. Rest 1
-            // i = 0, 1, 2, 3, 4, 5, , 0, 1 .... wiederholt die Zahlen von 0 bis 5
-            this.playAnimation(this.IMAGES_WALKING);
-        }, 300);  
-    }
-
-
+  imgAnimate() {
+    setInterval(() => {
+      this.chicken_sound.pause();
+      if (!this.isDead()) {
+        this.playAnimation(this.IMAGES_WALKING);
+      } else {
+        this.playAnimation(this.IMAGES_DEAD);
+        this.endPlayAnimation();
+      }
+    }, 120);
+  }
 }
